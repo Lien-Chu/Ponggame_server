65e08a2f0d22b30b1f17e439b658b7861d6b3f0e
LIEN-CHU HUANG <vickykilumanga@gmail.com>
2023-04-24 18:26:54 +0200

    let paddles moving

--- PongGameServer/wwwroot/javaScript/canvas.js
+++ PongGameServer/wwwroot/javaScript/canvas.js
@@ -5,14 +5,26 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 let _ballX = 320;
 let _ballY = 140;
-let _player1Y = 100;
-let _player2Y = 100;
-let ballDX = 5;
-let ballDY = 5;
+let ballDX = 3;
+let ballDY = 3;
 
+let paddle_l = {
+    x: 15,
+    y: 100,
+    width: 10,
+    height: 80,
+    speed: 20
+};
+let paddle_r = {
+    x: 615,
+    y: 100,
+    width: 10,
+    height: 80,
+    speed: 20
+};
 
 function draw() {
-
+    
     // Draw background
     ctx.clearRect(0, 0, 640, 280);
     ctx.strokeRect(0, 0, 640, 280);
@@ -30,9 +42,9 @@ function draw() {
     ctx.fill();
 
     // Draw paddles
-    ctx.fillStyle = "red";
-    ctx.fillRect(15, 100, 10, 80);
-    ctx.fillRect(615, 100, 10, 80);
+    ctx.fillStyle = "green";
+    ctx.fillRect(paddle_l.x, paddle_l.y, paddle_l.width, paddle_l.height);
+    ctx.fillRect(paddle_r.x, paddle_r.y, paddle_r.width, paddle_r.height);
 
     _ballX += ballDX;
     _ballY += ballDY;
@@ -46,23 +58,56 @@ function draw() {
         /* await hubConnection.SendAsync("MoveBall", _ballX, _ballY);*/
     }
     // Bounces when hitting on paddles
-    if (_ballX < 25 && _ballY > _player1Y && _ballY < _player1Y + 80) {
+    if (_ballX < 25 && _ballY > paddle_l.y && _ballY < paddle_l.y + 80) {
         ballDX = -ballDX;
     }
-    if (_ballX > 615 && _ballY > _player2Y && _ballY < _player2Y + 80) {
+    if (_ballX > 615 && _ballY > paddle_r.y && _ballY < paddle_r.y + 80) {
         ballDX = -ballDX;
     }
-
-
- 
-
     requestAnimationFrame(draw);
 }
 
 requestAnimationFrame(draw);
 
+document.addEventListener("keydown", function (event) {
+    if (event.code === "ArrowUp") {
+        paddle_l.y -= paddle_l.speed;
+    } else if (event.code === "ArrowDown") {
+        paddle_l.y += paddle_l.speed;
+    }
+});
+
+//document.addEventListener("keydown", function (event) {
+//    if (event.code === "ArrowUp") {
+//        hubConnection.send("MovePaddle", 1, paddle_l.y - paddle_l.speed);
+//    } else if (event.code === "ArrowDown") {
+//        hubConnection.send("MovePaddle", 1, paddle_l.y + paddle_l.speed)
+//    }
+//});
+
+//document.addEventListener("keydown", function (event) {
+//    if (event.code === "ArrowUp")
+//        hubConnection.send("MovePaddle", 2, paddle_r.y - paddle_r.speed);
+//    else if (event.code === "ArrowDown")
+//        hubConnection.send("MovePaddle", 2, paddle_r.y + paddle_r.speed);
+//});
 
 
 
+//private async Task OnKeyDown(KeyboardEventArgs e)
+//{
 
+//    if (e.Code == "ArrowUp") {
+//        if (hubConnection is not null)
+//        {
+//            await hubConnection.SendAsync("MovePaddle", 1, paddle_l - 20);
+//        }
+//    }
+//    else if (e.Code == "ArrowDown") {
+//        if (hubConnection is not null)
+//        {
+//            await hubConnection.SendAsync("MovePaddle", 1, paddle_l + 20);
+//        }
+//    }
+//}
 
